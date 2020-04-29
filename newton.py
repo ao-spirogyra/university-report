dbfe4f344949f81a1fc5a2c4e749a80aca0dc3c6
ao-spirogyra <lily.spirogyra@gmail.com>
2020-04-29 20:57:09 +0900

    add color

--- newton.py
+++ newton.py
@@ -15,14 +15,14 @@ def newton(initial_x):
     for i in range(max_loop):
         next_x = x_n[i] - func(x_n[i])/differentialFunc(x_n[i])
         x_n.append(next_x)
-        plt.plot(x_n[i],func(x_n[i]),marker = 'o')
+        plt.plot(x_n[i],func(x_n[i]),marker = 'o', color = 'g')
     return x_n
 
 if __name__ == '__main__':
     approximating_x = newton(2)
     x = np.linspace(-5,3,200)
     y = x ** 2 + 2*x + 1
-    plt.plot(x, y)
+    plt.plot(x, y, color = 'b')
     plt.xlabel('x')
     plt.ylabel('y', rotation = 0)
     plt.title('newton approximation')
